b39150ec5a2bad722a5eaae50faccd4232ee54f0
vyvy10 <79157046+vyvy10@users.noreply.github.com>
2021-03-26 18:18:13 -0400

    Update whale.js

--- whale.js
+++ whale.js
@@ -5,24 +5,41 @@ class Whale {
     this.y = height - this.r;
     this.vy = 0;
     this.gravity = 0.6;
-    
+    this.score = 0;
   }
-  
+
   jump() {
 
     this.vy = -15;
 
   }
-  hits(){
-    
+
+  hits(obs) {
+
+    if ((obs.x >= this.x && obs.x <= (this.x + this.r)) && (obs.y >= this.y && obs.y <= (this.y + this.r))) {
+     obs.y = -400;
+      this.score++;
+
+
+
+
+    }
   }
   
-  move() {
+  
+
+
+  move() { 
     this.y += this.vy;
     this.vy += this.gravity;
     this.y = constrain(this.y, 0, height - this.r);
   }
   show() {
-    image(wImg, this.x, this.y - 10, this.r + 20, this.r + 20);
-  }
+    image(wImg, this.x, this.y - 15, this.r + 35, this.r + 20);
+    fill(0, 0, 0);
+    stroke(225);
+    textSize(23);
+    textFont('serif');
+    text("Total Score:" + this.score,240, 60)
+  } 
 }
